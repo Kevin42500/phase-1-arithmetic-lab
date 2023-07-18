784e8f9d9c13e6f36db1ea1d968621e0f32c01fb
scobett <kevin@Scobett>
2023-07-18 12:25:54 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1; 
+console.log(random);
+
+const num3 = 40;
+const num4 = 9;
+const mod = num3 % num4;
+console.log(mod);
+
+const setOfNumbers = [5, 10, 15, 20, 7, 12];
+const max = Math.max(...setOfNumbers);
+console.log(max); 
